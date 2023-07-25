89bdfe019586df51bc05d4f3f835ffe5ef70b8f8
lat2k2 <lat2k2@mail.missouri.edu>
2023-07-24 22:49:05 -0500

    Fixing image on homepage

--- src/Page/home.js
+++ src/Page/home.js
@@ -1,11 +1,12 @@
 import React from "react";
 
+
 const Home =()=>{
     return(
         <div className="col-sm-8 text-left">
         <h2>Welcome to Hollow Knight!</h2>
         <p>Currently working on our page!</p>
-        <img src="./Images/Ghost.webp" alt="Ghost" width="300" height="300"/>
+        <img src={require('../Images/Ghost.webp')} alt="Ghost" width="300" height="300"/>
 
 
         <hr/>
